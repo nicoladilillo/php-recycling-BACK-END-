c8fd7553914920f03d9bd66cfa3a493955b49e0a
Dom <dom.corvasce@yandex.com>
2016-11-08 13:28:55 +0100

    Add favicon

--- partials/head.php
+++ partials/head.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
   <title><?php echo($title); ?></title>
+
   <link rel="stylesheet" href="/assets/css/main.css">
+  <link rel="icon" href="/assets/images/recycle.png">
+
   <script type="text/javascript" src="/assets/js/app.js" async></script>
 </head>
 <body>
