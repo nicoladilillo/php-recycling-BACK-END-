45daaad1833497b52b97c6396198095a0f7a1e1b
Dom <dom.corvasce@yandex.com>
2016-11-08 13:30:27 +0100

    Edit title in index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $title = 'Home'; require('./partials/head.php'); ?>
+<?php $title = 'DoveLoButto'; require('./partials/head.php'); ?>
 
 <p>CSS is <b id="not">not</b> working</p>
 <p>JS is <b id="js-not">not</b> working</p>
