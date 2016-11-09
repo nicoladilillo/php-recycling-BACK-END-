656909bbad2384c221eb2e990c621fdb11d83565
Dom <dom.corvasce@yandex.com>
2016-11-09 19:00:50 +0100

    Add initial sketch for response's page navbar

--- result.php
+++ result.php
@@ -1,6 +1,19 @@
 <?php $title = 'Result'; require('./partials/head.php'); ?>
 
 <div class="response-page plastic">
+  <div class="navbar">
+    <header>
+      <a href="/">
+        <img src="/assets/images/logo.svg" alt="DoveLoButto">
+        <span>DoveLoButto</span>
+      </a>
+    </header>
+    <form action="/result.php" method="get">
+      <input type="text" name="object">
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+
   <div id="response" class="lets-move pop-up-2s">
     <figure>
       <img src="/assets/images/bin-plastic.png" alt="Ricicla come plastica">
