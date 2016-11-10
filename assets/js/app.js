0665bd6f3982dc8df43e1b3844126923b1f6c7da
Dom <dom.corvasce@yandex.com>
2016-11-10 14:58:33 +0100

    Edit variable's names using camelCase convention

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,10 @@
   'use strict';
   
   // Control for empty input in the search bar.
-  var search_bar = document.querySelector(".search-bar");
+  var searchBar = document.querySelector(".search-bar");
   var input = document.querySelector(".search-bar input[type='text']");
 
-  search_bar.addEventListener("submit", function (e) {
+  searchBar.addEventListener("submit", function (e) {
     if (input.value === "") {
       e.preventDefault();
       input.className = "warning";
