dc790dd4f458f56c26d898640a3e2667473db7c6
Dom <dom.corvasce@yandex.com>
2016-11-10 14:57:00 +0100

    Add semi-finished homepage

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <?php $title = 'DoveLoButto'; require('./partials/head.php'); ?>
 
-<div class="action-box">
-  <article>
-    <p>Riciclare non è mai stato così facile..</p>
-  </article>
-  <form action="/result.php" method="get" class="search-bar">
-    <input type="text" name="object" placeholder="Ricicla ora">
-    <input type="image" src="/assets/images/action-icon.svg" alt="Ricicla">
-  </form>
+<div class="homepage">
+  <div class="navbar">
+    <header>
+      <a>
+        <img src="/assets/images/logo.svg" alt="DoveLoButto">
+        <span>DoveLoButto</span>
+      </a>
+    </header>
+  </div>
+
+  <div class="sidebar is-serif">
+    <article>
+      <p>
+        <strong>DoveLoButto</strong> ti permette di scoprire velocemente e 
+        facilmente come riciclare tutto ciò che ti passa per le mani.
+      </p>
+      <p>
+        La raccolta differenziata non è facile come sembra e questo servizio 
+        nasce apposta per facilitarla, fornendo alle persone uno strumento 
+        utile e facile da usare.
+      </p>
+    </article>
+  </div>
+
+  <div class="action-box">
+    <article>
+      <p>Riciclare non è mai stato così facile..</p>
+    </article>
+    <form action="/result.php" method="get" class="search-bar">
+      <input type="text" name="object" placeholder="Ricicla ora">
+      <input type="image" src="/assets/images/action-icon.svg" alt="Ricicla">
+    </form>
+  </div>
 </div>
 
 <?php require('./partials/tail.php'); ?>
