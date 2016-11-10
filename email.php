f282ef455425eb75aaf576da0d00c2fe217e243a
Dom <dom.corvasce@yandex.com>
2016-11-10 17:06:28 +0100

    Add initial sketch for e-mail confirmation's page

--- email.php
+++ email.php
@@ -1,7 +1,29 @@
 <?php $title = 'Email'; require('./partials/head.php'); ?>
 
-<p>
-  Ti invieremo al più presto un'email a: <b><?php echo($_POST['email']); ?></b>
-</p>
+<div class="email-page">
+  <div class="navbar">
+    <header>
+      <a href="/" class="is-centered">
+        <img src="/assets/images/logo.svg" alt="DoveLoButto">
+        <span>DoveLoButto</span>
+      </a>
+    </header>
+    <form action="/result.php" method="get" class="search-bar">
+      <input type="text" name="object" placeholder="Ricicla ora..">
+      <input type="image" src="/assets/images/action-icon.svg" alt="Ricicla">
+    </form>
+  </div>
+
+  <div id="content">
+    <figure>
+      <img src="/assets/images/check-icon.svg" alt="E-mail inviata">
+    </figure>
+
+    <article>
+      <p>Ti invieremo al più presto un'email a: 
+      <b></b></p>
+    </article>
+  </div>
+</div>
 
 <?php require('./partials/tail.php'); ?>
