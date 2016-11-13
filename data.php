f496b0b62f737a1f4bc47baf720815f94020dd36
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-11-13 09:37:41 +0100

    Add data.php

---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+
+const BLUE_BIN = 1;
+const RED_BIN = 2;
+const GREEN_BIN = 3;
+const BROWN_BIN = 4;
+
+$products = [
+"carta"    => BLUE_BIN,
+"vetro"    => GREEN_BIN,
+"plastica" => BROWN_BIN,
+"metallo"  => RED_BIN,
+"legno"    => BROWN_BIN
+];
+
+?>
